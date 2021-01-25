345448735559e76adf660058f80c15b373e34e62
rsarvar1a <rsarvaria@uwaterloo.ca>
2021-01-24 23:19:16 -0500

    feat(component): minimal interface :)

---
+++ src/game/components/component-base.py
@@ -0,0 +1,36 @@
+#
+#   Interface for game overlay objects.
+#
+class SHGameComponent (object):
+
+    #
+    # parent : SHGame
+    #   a ref to this component's parent
+    #
+    # client : DiscordClient
+    #   a ref to the bot client
+    #
+    def __init__(self, parent, client):
+        self.stage = None
+        self.parent = parent
+        self.client = client
+        # Anything else is component-specific.
+
+    #
+    # Runs once when the SHGame reaches this component's part of the flow.
+    #
+    def Setup(self):
+        pass
+
+    #
+    # Runs every time the bot receives any input from an event
+    # that occurs in the CategoryChannel owned by this component's 
+    # parent SHGame.
+    #
+    # context : tuple
+    #   (bundle, type), where bundle is the data from the client
+    #                   and type is its type as a string
+    #
+    def Handle(self, context):
+        pass
+
